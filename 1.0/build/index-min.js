31185c6e41837d3f0f83709c455272829a290387
shepherdwind <eward.song@gmail.com>
2014-02-17 15:55:17 +0800

    add build

---
+++ 1.0/build/index-min.js
@@ -0,0 +1,3 @@
+/*! kpmTest - v1.0 - 2014-02-17 3:55:06 PM
+* Copyright (c) 2014 shepherdwind; Licensed  */
+KISSY.add("gallery/kpmTest/1.0/index",function(a,b,c){function d(a){var b=this;d.superclass.constructor.call(b,a)}b.all;return a.extend(d,c,{},{ATTRS:{}}),d},{requires:["node","base"]});
